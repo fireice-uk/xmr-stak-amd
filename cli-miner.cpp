01240b11f500157598ed1c97a402941b36b33ba9
SChernykh <sergey.v.chernykh@gmail.com>
2017-07-17 21:15:09 +0200

    Initialize random number generator

--- cli-miner.cpp
+++ cli-miner.cpp
@@ -36,6 +36,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <ctime>
+
 #ifndef CONF_NO_TLS
 #include <openssl/ssl.h>
 #include <openssl/err.h>
@@ -68,6 +70,8 @@ int main(int argc, char *argv[])
 	SSL_load_error_strings();
 	OpenSSL_add_all_digests();
 #endif
+
+	std::srand(std::time(0));
 
 	const char* sFilename = "config.txt";
 	bool benchmark_mode = false;
